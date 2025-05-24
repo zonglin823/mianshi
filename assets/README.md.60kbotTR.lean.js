b3a8d01aee06425848ae9873fcea3895f2a8dcfc
unknown <2814335230@qq.com~>
2025-05-24 23:01:19 +0800

    add

---
+++ assets/README.md.60kbotTR.lean.js
@@ -0,0 +1,24 @@
+import{_ as s,c as n,a2 as i,o as e}from"./chunks/framework.CDk703ml.js";const k=JSON.parse('{"title":"面试派","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md"}'),p={name:"README.md"};function l(t,a,r,h,o,c){return e(),n("div",null,a[0]||(a[0]=[i(`<h1 id="面试派" tabindex="-1">面试派 <a class="header-anchor" href="#面试派" aria-label="Permalink to &quot;面试派&quot;">​</a></h1><p>系统专业的前端面试导航，网址 <a href="https://www.mianshipai.com/" target="_blank" rel="noreferrer">https://www.mianshipai.com/</a></p><ul><li><a href="https://juejin.cn/user/1714893868765373" target="_blank" rel="noreferrer">双越老师</a> 主导开发</li><li>大厂面试规范</li><li>多人共建，开源免费</li></ul><h2 id="贡献题目和答案" tabindex="-1">贡献题目和答案 <a class="header-anchor" href="#贡献题目和答案" aria-label="Permalink to &quot;贡献题目和答案&quot;">​</a></h2><p>欢迎提交 PR 来贡献题目和答案，同时你可以贴上自己的博客链接，我们相互导流，共同受益。</p><p>具体步骤如下。</p><h3 id="fork-这个项目" tabindex="-1">fork 这个项目 <a class="header-anchor" href="#fork-这个项目" aria-label="Permalink to &quot;fork 这个项目&quot;">​</a></h3><p>在 GitHub fork 这个项目到你的空间，然后 git clone 下载这个项目到本地，本地运行项目</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>npm i</span></span>
+<span class="line"><span>npm run docs:dev</span></span></code></pre></div><h3 id="修改代码" tabindex="-1">修改代码 <a class="header-anchor" href="#修改代码" aria-label="Permalink to &quot;修改代码&quot;">​</a></h3><p>可以新增题目，也可以修改现有题目的答案（如你有更好的答案）</p><p>每个题目的格式如下</p><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">## 前端常见的设计模式有哪些？以及应用场景</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">参考答案</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">::: details</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">（简明扼要的写出答案的核心内容，给出必要的解释、代码示例、截图）</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">（注意，这里不要写的太过于详细，篇幅适中，适合在面试中 2-3 分钟解答完毕）</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">（更详细的内容可写到你的博客里，贴到下面）</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">（最后，可以参考 AI 给出的答案，但不要无脑照抄 AI ，要对自己产出的内容负责）</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">参考资料 （如没有，就不写）</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">::: details</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 你的博客链接</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">:::</span></span></code></pre></div><p>PS. 如有图片，统一放在 <code>docs/imgs</code> 目录下。</p><h3 id="关于你的博客" tabindex="-1">关于你的博客 <a class="header-anchor" href="#关于你的博客" aria-label="Permalink to &quot;关于你的博客&quot;">​</a></h3><p>你的博客内容里一定要有到<a href="https://www.mianshipai.com/" target="_blank" rel="noreferrer">面试派</a>的导流文案，否则不予通过，我们是相互引流。</p><p>博客内容一定要和该题目相关，要保证质量和内容，不能为了引流和引流。</p><h3 id="提交-pr" tabindex="-1">提交 PR <a class="header-anchor" href="#提交-pr" aria-label="Permalink to &quot;提交 PR&quot;">​</a></h3><p>把修改的代码 git commit 和 push 到你自己的 GitHub 项目</p><p>然后提交 PR 到 <a href="https://github.com/mianshipai/mianshipai-web" target="_blank" rel="noreferrer">mianshipai-web</a> 的 <code>main</code> 分支，并添加 reviewer 审核人 <code>wangfupeng1988</code></p><p>管理员会每日查看 PR 并审核</p><h2 id="发布上线" tabindex="-1">发布上线 <a class="header-anchor" href="#发布上线" aria-label="Permalink to &quot;发布上线&quot;">​</a></h2><p>管理员会统一提交 PR ，把 <code>main</code> 合并到 <code>prod-deploy</code> 分支，合并后即可触发 <a href="https://github.com/mianshipai/mianshipai-web/actions" target="_blank" rel="noreferrer">actions</a> 并发布上线。</p>`,23)]))}const E=s(p,[["render",l]]);export{k as __pageData,E as default};
